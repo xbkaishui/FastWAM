edd7cc47316cd5c59413598af2848418b040176e
xbkaishui <xbkaishui@gmail.com>
2026-06-23 18:48:10 +0800

    add paomo dataset

--- gen_text_embd.sh
+++ gen_text_embd.sh
@@ -1,2 +1,3 @@
 # python scripts/precompute_text_embeds.py task=libero_uncond_2cam224_1e-4
- python scripts/precompute_text_embeds.py task=pangceban_uncond_2cam224_1e-4
+#  python scripts/precompute_text_embeds.py task=pangceban_uncond_2cam224_1e-4
+ python scripts/precompute_text_embeds.py task=paomo_uncond_2cam224_1e-4
---
+++ train_paomo.sh
@@ -0,0 +1,14 @@
+export TORCHINDUCTOR_MIX_ORDER_REDUCTION=0
+# export FASTWAM_SDPA_BACKEND=cudnn
+# export FASTWAM_CUDNN_BENCHMARK=1
+
+accelerate launch \
+  --config_file scripts/accelerate_configs/accelerate_zero2_ds_bucket200m.yaml \
+  --num_processes 6 \
+  scripts/train.py \
+  task=paomo_uncond_2cam224_1e-4 batch_size=20 num_workers=12 \
+  log_every=1 eval_every=999999 save_every=999999 \
+  seed=42 wandb.enabled=true
+  # ++encode_overlap=true ++encode_prefetch_factor=4 \
+  # ++dataloader_persistent_workers=true \
+  # ++compile_mot=false ++compile_vae=true ++compile_vae_mode=default
